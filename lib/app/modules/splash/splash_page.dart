32a7eeaff7e13bd31f0b084524a23f76825a1c5b
Giovani Fernandes <giovani_oli@hotmail.com>
2022-02-02 21:37:11 -0400

    widgets appbar, button and textformfield created

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:veggie_burguer_mobile/app/core/ui/widgets/veggie_app_bar.dart';
+import 'package:veggie_burguer_mobile/app/core/ui/widgets/veggie_button.dart';
+import 'package:veggie_burguer_mobile/app/core/ui/widgets/veggie_textformfield.dart';
 
 class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
 
-  const SplashPage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('splash'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: VeggieAppBar(),
+      body: Column(
+        children: [
+          const SizedBox(
+             height: 10,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: VeggieTextformfield(
+              label: 'Text Form',
+              obscureText: true,
+            ),
+          ),
+          const SizedBox(
+             height: 20,
+          ),
+          VeggieButton(label: 'Entrar', onPressed: (){})
+        ],
+      ),
+    );
   }
 }
