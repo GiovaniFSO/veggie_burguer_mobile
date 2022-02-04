6f3c63f7900d2a095a9235844fe328d196f19a1b
Giovani Fernandes <giovani_oli@hotmail.com>
2022-02-04 17:11:38 -0400

    add controller and register user

---
+++ lib/app/core/ui/widgets/veggie_state.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+abstract class VeggieState<S extends StatefulWidget, C extends GetxController> extends State<S>{
+  C get controller => Get.find();
+}
